b765e73c82fafca6a5688866a6eef57228cc2c70
Kamil Pirog <kamilpirog98@gmail.com>
2022-01-26 17:47:40 +0100

    Player Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float turningSpeed = 1.0f;
+
+    private float turningDirection;
+    private bool IsSpeeding => Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+    private bool IsTurningLeft => Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
+    private bool IsTurningRight => Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        if (IsTurningLeft)
+            turningDirection = 1.0f;
+        else if(IsTurningRight)
+            turningDirection = -1.0f;
+        else
+            turningDirection = 0.0f;
+    }
+    private void FixedUpdate()
+    {
+        if (IsSpeeding)
+            rb.AddForce(transform.up * speed);
+
+        if(turningDirection != 0f)
+            rb.AddTorque(turningDirection * turningSpeed);
+    }
+}
